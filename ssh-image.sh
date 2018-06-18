1bb10714aab484fb764b2ed91f8becfe9bd110ff
technik <technik7job@gmail.com>
2018-06-18 07:10:07 +0200

    ssh inspect

--- ssh-image.sh
+++ ssh-image.sh
@@ -1,4 +1,6 @@
 # docker run --name ssh -d -p 2200:22 -e SSH_USERPASS=secret fedora/ssh
 docker run -d -e SSH_USERPASS=secret fedora/ssh
-# ssh -p 2200 user@localhost
+
+# docker inspect <image> | grep IP
+# ssh -p 2200 user@172.17.0.2
 
