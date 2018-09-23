6934bb96c2b30d2e9a4aa352bec21b4e815f1147
Vitalii Cherkashyn <technik7job@gmail.com>
2018-09-23 21:07:51 +0200

    external folder for collections/core

--- solr/start.sh
+++ solr/start.sh
@@ -1,1 +1,4 @@
+# http://archive.apache.org/dist/lucene/solr/4.10.3/solr-4.10.3.tgz 
+# docker run -d --name=solr -p 8983:8983 -v /home/technik/temp/solr/example:/opt/solr/example solr:4.10.3
+
 docker run -d --name=solr -p 8983:8983 solr:4.10.3
