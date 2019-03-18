81a469107b6f82a4eb3d574aac6d613b41398df3
Vitalii Cherkashyn <technik7job@gmail.com>
2019-03-18 17:34:54 +0100

    run in virtual environment

---
+++ script-server/run-in-virtual-env.sh
@@ -0,0 +1,7 @@
+wget `curl -s https://api.github.com/repos/bugy/script-server/releases/latest | grep browser_download_url | cut -d '"' -f 4`
+unzip script-server.zip
+python3 -m venv virtual_env
+virtual_env/bin/pip install wheel 
+wget https://github.com/bugy/script-server/raw/master/requirements.txt
+virtual_env/bin/pip install -r requirements.txt
+virtual_env/bin/python3 launcher.py
