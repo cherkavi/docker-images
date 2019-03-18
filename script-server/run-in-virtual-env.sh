40cdc7e71c1ffcf5d99ffe2033c878d79f5d9e5d
Vitalii Cherkashyn <technik7job@gmail.com>
2019-03-18 17:40:02 +0100

    cleanup before running

--- script-server/run-in-virtual-env.sh
+++ script-server/run-in-virtual-env.sh
@@ -1,3 +1,4 @@
+rm -rf  -- !(run-in-virtual-env.sh)
 wget `curl -s https://api.github.com/repos/bugy/script-server/releases/latest | grep browser_download_url | cut -d '"' -f 4`
 unzip script-server.zip
 python3 -m venv virtual_env
