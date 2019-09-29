2842d11978d90767e8dd47c44c6e886af4fe58d4
Vitalii Cherkashyn <vitalii.cherkashyn@bmw.de>
2019-09-29 11:06:21 +0200

    persistence volume

--- redis/create-redis-server.sh
+++ redis/create-redis-server.sh
@@ -1,2 +1,12 @@
 #!/bin/sh
-sudo docker run --publish 6379:6379 --volume data:/data --name redis-lab --detach redis
+#
+# https://hub.docker.com/_/redis
+#
+# start redis with persistent storage
+# with persistent volume
+# ```
+sudo docker run --publish 6379:6379 --volume `pwd dirname $0`/data:/data --name redis-lab --detach redis redis-server --appendonly yes
+# ```
+# 
+# start without persistence
+# sudo docker run --publish 6379:6379 --name redis-lab --detach redis
