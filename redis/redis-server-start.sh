dbcf26928aad62c7e0038c454aef291f0edc19c0
Vitalii Cherkashyn <vitalii.cherkashyn@bmw.de>
2019-10-13 16:14:28 +0200

    decrease amount of scripts

---
+++ redis/redis-server-start.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+if ! docker ps -a --filter "name=redis-lab" --format "{{.Names}}"
+then
+    #
+    # https://hub.docker.com/_/redis
+    #
+    # start redis with persistent storage
+    # with persistent volume
+    # ```
+    sudo docker run --publish 6379:6379 --volume `pwd dirname $0`/data:/data --name redis-lab --detach redis redis-server --appendonly yes
+    # ```
+    # 
+    # start without persistence
+    # sudo docker run --publish 6379:6379 --name redis-lab --detach redis
+else
+    sudo docker start redis-lab
+    sleep 2
+    docker ps  
+fi
--- redis/start-existing-redis-server.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-sudo docker start redis-lab
-sleep 2
-docker ps
