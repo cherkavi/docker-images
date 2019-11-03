2bf4ba283fc0753433092b784eeb1e74cde10b00
Vitalii Cherkashyn <technik7job@gmail.com>
2019-11-03 12:50:25 +0100

    start existing

--- jenkins/jenkins-image.sh
+++ jenkins/jenkins-image.sh
@@ -1,5 +1,13 @@
-# docker volume create {volume name}
-# docker pull jenkins/jenkins:lts
-docker run -p 8080:8080 -p 50000:50000 -v jenkins_home:/var/jenkins_home jenkins/jenkins:lts
-# docker run --user root -p 8080:8080 -p 50000:50000 -v jenkins_home:/var/jenkins_home jenkins/jenkins:lts
-# /var/jenkins_home/secrets/initialAdminPassword
+
+
+started_container=`docker ps -a --filter "name=^jenkins-server$" --format "{{.Names}}"`
+if [ -z "$started_container" ]
+then
+  docker run -p 8080:8080 -p 50000:50000 -v `realpath .`/data:/var/jenkins_home  --name jenkins-server  jenkins/jenkins:lts
+  # docker run --user root -p 8080:8080 -p 50000:50000 -v jenkins_home:/var/jenkins_home jenkins/jenkins:lts
+  # /var/jenkins_home/secrets/initialAdminPassword
+else
+  sudo docker start jenkins-server
+  sleep 3
+  docker ps
+fi
