0bbb7c08acb140d171b54d58e4a2e3c03ac6cbc4
Vitalii Cherkashyn <technik7job@gmail.com>
2019-11-14 19:38:49 +0100

    prometheus server

---
+++ prometheus/prometheus.sh
@@ -0,0 +1,1 @@
+docker run --rm -p 9090:9090 -v prometheus.yml:/etc/prometheus/prometheus.yml -v $HOME/temp/prometheus-data:/usr/local/var/prometheus --name prometheus prom/prometheus --config.file=/etc/prometheus/prometheus.yml --storage.tsdb.path=/usr/local/var/prometheus
