f39ddbdc79219cae7b9dc411484389923e94ae8a
Vitalii Cherkashyn <technik7job@gmail.com>
2019-11-15 07:23:25 +0100

    automation for start/re-start

--- postgresql/postgres-server.sh
+++ postgresql/postgres-server.sh
@@ -1,9 +1,17 @@
 container_name="postgres-docker"
+started_container=`docker ps -a --filter "name=^postgres-docker$" --format "{{.Names}}"`
+
 master_password="docker"
 external_port=5432
-external_folder=$HOME/temp/volumes/postgres
+external_folder=`pwd dirname $0`/data
 
-docker run --rm --name $container_name -e POSTGRES_PASSWORD=$master_password -d -p $external_port:5432 -v $external_folder:/var/lib/postgresql/data  postgres
 
-# client, use $master_password
-# docker run -it --rm --network="host" postgres psql -h 0.0.0.0 -U postgres
+if [ -z "$started_container" ]
+then
+    sudo docker run --rm --name $container_name -e POSTGRES_PASSWORD=$master_password -d -p $external_port:5432 -v $external_folder:/var/lib/postgresql/data  postgres
+    echo "docker run -it --rm --network='host' postgres psql -h 0.0.0.0 -U postgres"
+else
+    sudo docker start $container_name
+    sleep 2
+    docker ps  
+fi
