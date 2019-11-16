ace53a56cbc13bb020e403fcdd1620f41236366a
Vitalii Cherkashyn <technik7job@gmail.com>
2019-11-16 09:34:26 +0100

    clean up and re-arrange

--- prometheus/prometheus.sh
+++ prometheus/prometheus.sh
@@ -1,1 +1,9 @@
-docker run --rm -p 9090:9090 -v prometheus.yml:/etc/prometheus/prometheus.yml -v $HOME/temp/prometheus-data:/usr/local/var/prometheus --name prometheus prom/prometheus --config.file=/etc/prometheus/prometheus.yml --storage.tsdb.path=/usr/local/var/prometheus
+echo "sometimes need to create ./data folder and give it full access 777 "
+docker run --rm \
+        -p 9090:9090 \
+	-v `pwd dirname $0`/config:/etc/prometheus  \
+	-v `pwd dirname $0`/data:/usr/local/var/prometheus \
+	--name prometheus \
+	prom/prometheus \
+	--config.file=/etc/prometheus/prometheus.yml \
+	--storage.tsdb.path=/usr/local/var/prometheus
