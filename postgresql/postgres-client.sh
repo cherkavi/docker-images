386ae0ba11ebe94ed77b3b2b26494fc8f7d56c42
Vitalii Cherkashyn <technik7job@gmail.com>
2019-11-18 18:35:24 +0100

    automation and client

---
+++ postgresql/postgres-client.sh
@@ -0,0 +1,1 @@
+docker run -it --rm --network='host' postgres psql -h 0.0.0.0 -p 5432 -U postgres
--- postgresql/postgres-server.sh
+++ postgresql/postgres-server.sh
@@ -1,17 +1,64 @@
-container_name="postgres-docker"
-started_container=`docker ps -a --filter "name=^postgres-docker$" --format "{{.Names}}"`
+container_name="postgres-server"
+port=5432
+password="postgres"
+# default DB name
+database_name="postgres"
 
-master_password="docker"
-external_port=5432
-external_folder=`pwd dirname $0`/data
+connection_string(){
+    echo "connection string: ""postgres://postgres:$password@127.0.0.1:$port/$database_name"
+}
 
 
-if [ -z "$started_container" ]
-then
-    sudo docker run --rm --name $container_name -e POSTGRES_PASSWORD=$master_password -d -p $external_port:5432 -v $external_folder:/var/lib/postgresql/data  postgres
-    echo "docker run -it --rm --network='host' postgres psql -h 0.0.0.0 -U postgres"
-else
-    sudo docker start $container_name
-    sleep 2
-    docker ps  
-fi
+start() {
+    persistent_folder=`pwd dirname $0`/data
+    started_container=`docker ps -a --filter "name=^postgres-server$" --format "{{.Names}}"`
+    if [ -z "$started_container" ]
+    then
+        docker run --rm   --name $container_name -e POSTGRES_PASSWORD=$password -d -p $port:5432 -v $persistent_folder:/var/lib/postgresql/data  postgres
+    else
+        docker start $container_name
+    fi
+    echo "started"
+    status
+}
+
+
+
+status() {
+    started_container=`docker ps --filter "name=^postgres-server$" --format "{{.Names}}"`
+    if [ -z "$started_container" ]
+    then
+        echo "was not started"        
+    else
+        echo "running..."
+        connection_string
+    fi
+}
+
+stop() {
+    docker stop $container_name
+    echo "was stopped"
+}
+
+
+case "$1" in
+    'start')
+            start
+            ;;
+    'stop')
+            stop
+            ;;
+    'restart')
+            stop ; echo "Sleeping..."; sleep 1 ;
+            start
+            ;;
+    'status')
+            status
+            ;;
+    *)
+            echo
+            echo "Usage: $0 { start | stop | restart | status }"
+            echo
+            exit 1
+            ;;
+esac
