4ec07f2e5947196cbaee92ee515b17033a53d20a
Vitalii Cherkashyn <technik7job@gmail.com>
2019-11-24 12:17:14 +0100

    cleanup bash file

--- cAdvisor/cAdvisor-start.sh
+++
@@ -1,1 +0,0 @@
-sudo docker run --volume=/var/run:/var/run:rw --volume=/sys:/sys:ro --volume=/var/lib/docker/:/var/lib/docker:ro --publish=9090:8080 --detach=false --name=cadvisor google/cadvisor:latest
