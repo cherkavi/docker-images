c7a6ded38ecfca04307b182d9f8a5f883a9e5cde
Vitalii Cherkashyn <technik7job@gmail.com>
2019-11-24 13:45:03 +0100

    cleanup code

--- jenkins/jenkins-image.sh
+++ jenkins/jenkins-image.sh
@@ -3,8 +3,8 @@
 started_container=`docker ps -a --filter "name=^jenkins-server$" --format "{{.Names}}"`
 if [ -z "$started_container" ]
 then
-  docker run -p 8080:8080 -p 50000:50000 -v `realpath .`/data:/var/jenkins_home  --name jenkins-server  jenkins/jenkins:lts
-  # docker run --user root -p 8080:8080 -p 50000:50000 -v jenkins_home:/var/jenkins_home jenkins/jenkins:lts
+  docker run -p 8080:8080 -p 50000:50000 -v `pwd dirname $0`/data:/var/jenkins_home  --name jenkins-server  jenkins/jenkins:lts
+  # docker run --user root -p 8080:8080 -p 50000:50000 -v `pwd dirname $0`/jenkins_home:/var/jenkins_home jenkins/jenkins:lts
   # /var/jenkins_home/secrets/initialAdminPassword
 else
   sudo docker start jenkins-server
