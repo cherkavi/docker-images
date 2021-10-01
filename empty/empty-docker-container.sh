da4cbb20cfd6e36b16ed76f762110000e9f6da52
Vitalii Cherkashyn <vitalii.cherkashyn@gmail.com>
2021-10-01 14:03:48 +0200

    current folder mapping

--- empty/empty-docker-container.sh
+++ empty/empty-docker-container.sh
@@ -1,2 +1,5 @@
 # docker run --name ssh -d -p 2200:22 -e SSH_USERPASS=secret fedora/ssh
 docker run -it ubuntu:18.04 /bin/bash
+
+# empty container with empty entry point and with current folder mapping
+docker run --entrypoint="" --volume $(pwd):/host_folder --rm -it maprtech/pacc:6.1.0_6.0.0_ubuntu16 /bin/bash
